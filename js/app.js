585b38092bb3858f44d1f5204754b356c7614bd8
packdproject <connecttodell@gmail.com>
2026-06-11 01:59:00 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 async function initApp() {
-  await loadSidebar();
-  await loadDashboard();
-  initSearch();
+    await loadSidebar();
+    await loadDashboard();
+    initSearch();
 }
 
-// Global functions for HTML onclick
+// Register global functions for HTML onclick
 window.changePage = changePage;
 window.showDetail = showDetail;
 window.loadSheet = loadSheet;
@@ -12,5 +12,5 @@ window.loadDashboard = loadDashboard;
 window.showSearchDetail = showSearchDetail;
 
 document.addEventListener("DOMContentLoaded", () => {
-  initApp();
+    initApp();
 });
