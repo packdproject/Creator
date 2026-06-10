6ad91152f04e8007f877f204edec962be1a61482
packdproject <connecttodell@gmail.com>
2026-06-11 02:27:51 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxtkfn059eEdHCPKPeYMu5UL0HyAYtMz7MWb5kQoqmJM1PIDG1RA64AxArCAqQLs_IG/exec";
+// config.js - Kosongkan atau hapus file ini
+// API_URL sudah didefinisikan di api.js
