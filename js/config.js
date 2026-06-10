8d96159def3a05068a9fbdf739110dad2023bf81
packdproject <connecttodell@gmail.com>
2026-06-11 02:30:05 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,7 @@
-// config.js - Kosongkan atau hapus file ini
-// API_URL sudah didefinisikan di api.js
+// Google Apps Script API URL
+const API_URL = "https://script.google.com/macros/s/AKfycbxtkfn059eEdHCPKPeYMu5UL0HyAYtMz7MWb5kQoqmJM1PIDG1RA64AxArCAqQLs_IG/exec";
+
+// App configuration
+const APP_NAME = "Creator Vault";
+const APP_VERSION = "2.0";
+const ITEMS_PER_PAGE = 20;
