b58ee809960b946c8fcae47014f2a05b7b7f1459
packdproject <connecttodell@gmail.com>
2026-06-11 22:09:54 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,1 @@
-// Google Apps Script API URL
 const API_URL = "https://script.google.com/macros/s/AKfycbxtkfn059eEdHCPKPeYMu5UL0HyAYtMz7MWb5kQoqmJM1PIDG1RA64AxArCAqQLs_IG/exec";
-
-// App configuration
-const APP_NAME = "Creator Vault";
-const CACHE_DURATION = 5 * 60 * 1000; // 5 menit cache
